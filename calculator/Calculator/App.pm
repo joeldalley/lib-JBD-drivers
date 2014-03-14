554f349deccf7608d057beaecf58445ddbe43109
Joel Dalley <joeldalley@gmail.com>
2014-03-14 08:16:03 -0600

    development

--- calculator/Calculator/App.pm
+++ calculator/Calculator/App.pm
@@ -5,122 +5,19 @@ package Calculator::App;
 # @author Joel Dalley
 # @version 2014/Feb/28
 
-use Carp 'croak';
-use JBD::Core::Exporter ':omni';
-use JBD::Core::List qw(zip pairsof);
-
 use JBD::Parser::DSL;
-use Calculator::Grammar 
-    qw(expr term factor types op primitive_operators);
+use Calculator::Grammar qw(expr types);
+use JBD::Core::Exporter ':omni';
 
-use constant Nicht => token Nothing;
-use constant EOI   => token End_of_Input;
-use constant Prims => primitive_operators;
-
-# Replace two subs with ones that compute intermediate
-# values for sub-expressions; this way, we control
-# the order of evaluation by ensuring that 
-# parenthesized sub-expressions are evaulated first,
-# and that a sequence of * and/or / Ops is evaluated
-# from left to right, and thus, ultimately, that each
-# full expression, $EXPR, evaluates to the same value 
-# that eval($EXPR) would produce.
-
-Calculator::Grammar::init(
-    enclosed_expr => sub {
-        my $p = op '(' ^ expr ^ op ')';
-
-        parser { 
-            my $in = shift;
-            my ($tok) = $p->($in);
-            replace($tok, $in);
-        };
-    },
-    fact_op_term => sub {
-        my $f = factor;
-        my $p = $f ^ star((op '*' | op '/') ^ ($f | term));
-
-        parser {
-            my $in = shift;
-            my ($tok) = $p->($in);
-            replace($tok, $in);
-        };
-    }
-); 
-
-
-#///////////////////////////////////////////////////////////////
-# Interface ////////////////////////////////////////////////////
-
-# @param string $text User input, e.g., '3 * (.5 + 2) * -2'
+# @param string $text User input, e.g., '3 + (.24 * -2)'
 # @return scalar Calculation result.
 sub calculate($) {
-    my $text   = shift;
-    my $lexed  = tokens $text, [types, Op];
-    my $state  = parser_state [@$lexed, token End_of_Input];
-    my $parsed = (expr ^ type End_of_Input)->($state)
-                 or croak $state->error_string;
-    compute("calculate($text)", $parsed)->value;
-}
-
-
-#///////////////////////////////////////////////////////////////
-#/ Internally used /////////////////////////////////////////////
-
-# @param string $caller For the error message, if needed.
-# @param arrayref $tokens JBD::Parser::Tokens.
-# @return A JBD::Parser::Token containing the computed value.
-sub compute($$) {
-    my ($caller, $tokens) = @_;
-
-    # Perl primitive arithmetic ops.
-    my %math = (
-        '+' => sub { $_[0] + $_[1] },
-        '-' => sub { $_[0] - $_[1] },
-        '*' => sub { $_[0] * $_[1] },
-        '/' => sub { $_[0] / $_[1] }
-        );
-
-    # Compute.
-    my @values = grep defined $_, map $_->value, @$tokens;
-    my ($val, $pairs) = (shift @values, pairsof @values);
-    while (my $pair = $pairs->()) {
-        my ($op, $num) = @$pair;
-        die "Illegal division by zero `$val/$num`"
-            if $op eq '/' && $num == 0;
-        $val = $math{$op}->($val, $num);
-    }
-    my $res = tokens $val, [types] if defined $val;
-
-    # Ensure computation is closed under %math ops.
-    die 'Range error: undefined value' if !defined $val;
-    die "Range error: no token for `$val`" if !$res;
-
-    shift @$res;
-}
-
-# @param arrayref $tok JBD::Parser::Tokens.
-# @param JBD::Parser:Input $in.
-# @return mixed Array of ([JBD::Parser::Tokens], JBD::Parser::Input),
-#               or undef (on failure to parse).
-sub replace {
-    my ($tok, $in) = @_;
-
-    if (ref $tok) {
-        my @rators = grep $_->anyof([Op], [Prims]), @$tok;
-        my @rands  = grep $_->typeis(types), @$tok;
-        my $res = $rands[0];
-
-        if (@rators) {
-            my $str = join ' ', map "[$_]", @$tok;
-            my $expr_tok = [grep $_, zip @rands, @rators];
-            $res = compute("replace($str)", $expr_tok);
-        }
-
-        return ([$res, map Nicht, (0 .. @$tok-2)], $in);
-    }
-
-    undef;
+    my $text = shift;
+    my $lexed = tokens $text, [types, Op];
+    my $state = parser_state [@$lexed, token End_of_Input];
+    my $parsed = expr->($state) or die $state->error_string;
+    return shift(@$parsed)->value if @$parsed == 2;
+    die "Expecting result token & end of input token only";
 }
 
 1;
--- calculator/Calculator/Grammar.pm
+++ calculator/Calculator/Grammar.pm
@@ -1,54 +1,26 @@
 package Calculator::Grammar;
 
-# Defines a grammar, and provides subs for each production.
+# Define grammar.
 # @author Joel Dalley
 # @version 2014/Feb/27
 
 use JBD::Parser::DSL;
 use JBD::Core::Exporter ':omni';
 
-my ($E, $T, $F);
+sub op($)     { pair Op, shift }
+sub types()   { Signed, Unsigned }
+sub operand() { any map type $_, types }
 
-# Calculator operand types.
-sub types() { Signed, Unsigned }
+my $atom = operand;
+my $term  = $atom ^ star((op '+' | op '*') ^ $atom);
 
-# Calulator operators.
-sub primitive_operators() { qw(+ - * /) }
+my $expr = trans $term, sub {
+    my $tokens = shift;
+    my $expr = join ' ', grep $_, map $_->value, @$tokens;
+    my $val = eval $expr; die $@ if $@;
+    tokens $val, [types];
+};
 
-
-# Grammatical productions.
-sub expr()          { $E }
-sub term()          { $T }
-sub factor()        { $F }
-sub op(;$)          { pair Op, shift }
-sub operand()       { any map type $_, types }
-sub enclosed_expr() { op '(' ^ $E ^ op ')' }
-sub term_op_expr()  { $T ^ (op '+' | op '-') ^ $E }
-sub fact_op_term()  { $F ^ (op '*' | op '/') ^ $T }
-
-
-# Initialize parsers and define grammar rules.
-# @param hash [opt] %replace Map of {symbol => coderef}.
-sub init(%) {
-    my %replace = @_;
-
-    # Replacer. 
-    my $def = sub {
-        no strict 'refs';
-        my $sub = shift;
-        $replace{$sub} ? &{$replace{$sub}} : &$sub;
-    };
-
-    # Parsers.
-    my ($expr, $term, $fact);
-    $E = parser { $expr->(@_) };
-    $T = parser { $term->(@_) };
-    $F = parser { $fact->(@_) };
-
-    # Grammar rules.
-    $expr = $def->('term_op_expr') | $T;
-    $term = $def->('fact_op_term') | $F;
-    $fact = operand | $def->('enclosed_expr');
-}
+sub expr() { $expr ^ type End_of_Input }
 
 1;
