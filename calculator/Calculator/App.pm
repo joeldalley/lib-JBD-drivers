bef6c54d266415352756b54e20eb3b4b0f5a1bf3
Joel Dalley <joeldalley@gmail.com>
2014-03-15 12:34:05 -0600

    dev

--- calculator/Calculator/App.pm
+++ calculator/Calculator/App.pm
@@ -6,20 +6,46 @@ package Calculator::App;
 # @version 2014/Feb/28
 
 use JBD::Parser::DSL;
-use Calculator::Grammar qw(expr types);
+use Calculator::Grammar qw(init types operators expr);
 use JBD::Core::Exporter ':omni';
+use JBD::Core::List 'pairsof';
+
+our %math = (
+    '+' => sub {$_[0] + $_[1]},
+    '-' => sub {$_[0] - $_[1]},
+    '*' => sub {$_[0] * $_[1]},
+    '/' => sub {$_[0] / $_[1]}
+    );
+
+init Expr     => \&reduce_expr,
+     enclosed => \&reduce_enclosed;
+
+
+# @param arrayref Array of JBD::Parser::Tokens.
+# @return arrayref Token values, for defined values.
+sub phrase($) { 
+    [map $_->value, grep defined $_->value, @{$_[0]}];
+}
 
 # @param arrayref $tokens Array of JBD::Parser::Tokens.
 # @return mixed A result JBD::Parser::Token, or undef.
-sub reduce($) {
-    my $tokens = shift;
-    my $expr = join ' ', grep defined $_,
-               map $_->value, @$tokens;
-    my $val = eval $expr; 
-    die $@ if $@;
-    my $res = tokens $val, [types];
-    ref $res ? shift @$res : undef;
+sub reduce_expr($) {
+    my $phrase = phrase shift;
+
+    # Crunch numbers.
+    my $value = shift @$phrase;
+    my $pairs = pairsof @$phrase;
+    while (my $pair = $pairs->()) {
+        my ($op, $num) = @$pair;
+        $value = $math{$op}->($value, $num);
+    }
+
+    tokens $value, [types];
 }
+
+# @param arrayref $tokens Array of JBD::Parser::Tokens.
+# @return arrayref 1-element array. The middle token.
+sub reduce_enclosed($) { reduce_expr [shift->[1]] }
 
 # @param string $text User input, e.g., '3 + (.24 * -2)'
 # @return scalar Calculation result.
@@ -30,10 +56,7 @@ sub calculate($) {
     my $state = parser_state [@$lexed, token End_of_Input];
     my $parsed = (expr ^ type End_of_Input)->($state) 
         or die $state->error_string;
-
-    my $result = reduce $parsed 
-        or die qq|Unable to reduce "@$parsed"|;
-    $result->value;
+    shift(@$parsed)->value;
 }
 
 1;
--- calculator/Calculator/Grammar.pm
+++ calculator/Calculator/Grammar.pm
@@ -11,14 +11,26 @@ my ($Atom, $Expr);
 my $atom = parser {$Atom->(@_)};
 my $expr = parser {$Expr->(@_)};
 
-sub types()    { Signed, Unsigned }
-sub num()      { type Unsigned | type Signed }
-sub combine()  { any map pair(Op, $_), qw(+ - * /) }
-sub enclosed() { pair(Op, '(') ^ $expr ^ pair(Op, ')') }
+sub types()     { Signed, Unsigned }
+sub operators() { qw(+ - * /) }
+sub num()       { type Unsigned | type Signed }
+sub op($)       { pair Op, shift }
+sub operator()  { any map op $_, operators }
+sub enclosed()  { op '(' ^ $expr ^ op ')' }
+sub Expr()      { $atom ^ star(operator ^ $atom) }
+sub expr()      { $Expr }
 
-$Atom = num | enclosed;
-$Expr = $atom ^ star(combine ^ $atom);
+sub init(%) {
+    my %trans = @_;
 
-sub expr() { $Expr }
+    my $def = sub {
+        no strict 'refs';
+        my $sub = shift;
+        $trans{$sub} ? trans &$sub, $trans{$sub} : &$sub;
+    };
+
+    $Atom = num | $def->('enclosed');
+    $Expr = $def->('Expr');
+}
 
 1;
