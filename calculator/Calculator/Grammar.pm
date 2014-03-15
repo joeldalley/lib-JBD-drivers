de0a1db51b469290c3e4e87b750106adf17f603b
Joel Dalley <joeldalley@gmail.com>
2014-03-15 10:46:56 -0600

    dev

--- calculator/Calculator/Grammar.pm
+++ calculator/Calculator/Grammar.pm
@@ -7,15 +7,17 @@ package Calculator::Grammar;
 use JBD::Parser::DSL;
 use JBD::Core::Exporter ':omni';
 
-sub types() { Signed, Unsigned }
-sub num()   { type Unsigned | type Signed }
-sub R()     { any map pair(Op, $_), qw(+ - * /) }
-
 my ($Atom, $Expr);
 my $atom = parser {$Atom->(@_)};
 my $expr = parser {$Expr->(@_)};
-$Atom = num | (pair(Op, '(') ^ $expr ^ pair(Op, ')'));
-$Expr = $atom ^ star(R ^ $atom);
+
+sub types()    { Signed, Unsigned }
+sub num()      { type Unsigned | type Signed }
+sub combine()  { any map pair(Op, $_), qw(+ - * /) }
+sub enclosed() { pair(Op, '(') ^ $expr ^ pair(Op, ')') }
+
+$Atom = num | enclosed;
+$Expr = $atom ^ star(combine ^ $atom);
 
 sub expr() { $Expr }
 
