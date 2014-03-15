836f9d51f49c0b9cb82ccfb4243a996130caa0a3
Joel Dalley <joeldalley@gmail.com>
2014-03-15 10:42:14 -0600

    development

--- calculator/Calculator/App.pm
+++ calculator/Calculator/App.pm
@@ -9,16 +9,31 @@ use JBD::Parser::DSL;
 use Calculator::Grammar qw(expr types);
 use JBD::Core::Exporter ':omni';
 
+# @param arrayref $tokens Array of JBD::Parser::Tokens.
+# @return mixed A result JBD::Parser::Token, or undef.
+sub reduce($) {
+    my $tokens = shift;
+    my $expr = join ' ', grep defined $_,
+               map $_->value, @$tokens;
+    my $val = eval $expr; 
+    die $@ if $@;
+    my $res = tokens $val, [types];
+    ref $res ? shift @$res : undef;
+}
+
 # @param string $text User input, e.g., '3 + (.24 * -2)'
 # @return scalar Calculation result.
 sub calculate($) {
     my $text = shift;
+
     my $lexed = tokens $text, [types, Op];
     my $state = parser_state [@$lexed, token End_of_Input];
     my $parsed = (expr ^ type End_of_Input)->($state) 
         or die $state->error_string;
-    return shift(@$parsed)->value if @$parsed == 2;
-    die "Expecting result token & end of input token only";
+
+    my $result = reduce $parsed 
+        or die qq|Unable to reduce "@$parsed"|;
+    $result->value;
 }
 
 1;
--- calculator/Calculator/Grammar.pm
+++ calculator/Calculator/Grammar.pm
@@ -14,18 +14,9 @@ sub R()     { any map pair(Op, $_), qw(+ - * /) }
 my ($Atom, $Expr);
 my $atom = parser {$Atom->(@_)};
 my $expr = parser {$Expr->(@_)};
-
 $Atom = num | (pair(Op, '(') ^ $expr ^ pair(Op, ')'));
 $Expr = $atom ^ star(R ^ $atom);
 
-sub expr() {
-    trans $Expr, sub {
-        my $tokens = shift;
-        my $expr = join ' ', grep defined $_, 
-                   map $_->value, @$tokens;
-        my $val = eval $expr; die $@ if $@;
-        tokens $val, [types];
-    }
-}
+sub expr() { $Expr }
 
 1;
