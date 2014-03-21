1720470372a8612ceecaedcc9ad1d4b186cf6246
Joel Dalley <joeldalley@gmail.com>
2014-03-21 06:54:17 -0600

    dev

--- core/display.pl
+++ core/display.pl
@@ -1,7 +1,7 @@
 use JBD::Core::stern;
 use JBD::Core::Display;
 
-my $p = '/home/joel/projects/website-JBD/hosted_files/subdomains/tempo/tmpl';
+my $p = '../../website-JBD/hosted_files/subdomains/tempo/tmpl';
 my $display = JBD::Core::Display->new($p, '<!--HREF-->' => '/tempo');
 
 print $display->('h1.html', '<!--H1-->' => 'Hello');
